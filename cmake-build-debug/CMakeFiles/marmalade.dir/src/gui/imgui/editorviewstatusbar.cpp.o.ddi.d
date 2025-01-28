7a5027c6d983ca10885b3f98001c37023500afd1
Tayler Parsons <reeceparsons666@gmail.com>
2025-01-28 16:59:48 +0000

    Add .gitignore

--- cmake-build-debug/CMakeFiles/marmalade.dir/src/gui/imgui/editorviewstatusbar.cpp.o.ddi.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/marmalade.dir/src/gui/imgui/editorviewstatusbar.cpp.o.ddi: \
- /home/tayler/projects/marmalade/src/gui/imgui/editorviewstatusbar.cpp \
- /usr/include/stdc-predef.h \
- /home/tayler/projects/marmalade/src/gui/imgui/../../gui/imgui/editorviewstatusbar.h
