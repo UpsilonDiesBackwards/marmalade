b8565a4bd0c4410a71131ca54168aff7983a3339
Ryan Bester <ryan.bester5@gmail.com>
2025-02-03 17:05:50 +0000

    Add window.h docs.

--- src/gui/window.h
+++ src/gui/window.h
@@ -1,22 +1,42 @@
-#ifndef MARMALADE_WINDOW_H
-#define MARMALADE_WINDOW_H
+#ifndef MARMALADE_GUI_WINDOW_H
+#define MARMALADE_GUI_WINDOW_H
 
 namespace Marmalade::GUI {
+    /**
+     * \brief Abstract class to control an ImGui window.
+     */
     class Window {
     public:
+        /**
+         * \brief The visibility state of the window.
+         */
         bool visible{false};
 
         Window() = default;
         explicit Window(bool visible) : visible(visible) {};
         virtual ~Window() = default;
 
+        /**
+         * \brief Draws the window.
+         *
+         * Will only be called if Window#visible is true.
+         * This should contain the `ImGui::Begin` function with `&visible` as the `p_open` parameter.
+         */
         virtual void Draw() = 0;
 
+        /**
+         * \brief Shows the window if should be shown.
+         *
+         * Wrapper for the Window#Draw method, but only calls if Window#visible is true.
+         */
         inline void Show() {
             if (!visible) return;
             Draw();
         }
 
+        /**
+         * \brief Toggles the visibility of the window.
+         */
         inline void ToggleWindow() {
             visible = !visible;
         }
