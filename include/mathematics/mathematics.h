e97f24cc382c3c7f7de349dc1ceb3ccc6aec64b3
Tayler Parsons <reeceparsons666@gmail.com>
2025-02-08 18:24:53 +0000

    Add Vec3 & Vec4 to Mathematics header file
    Create Vector 3 and Vector 4 types in Mathematics header file

--- include/mathematics/mathematics.h
+++ include/mathematics/mathematics.h
@@ -3,6 +3,8 @@
 #define MARMALADE_MATHEMATICS_H
 
 #include "types/vector2.h"
+#include "types/vector3.h"
+#include "types/vector4.h"
 
 namespace Marmalade::Mathematics {
        class Mathematics {
